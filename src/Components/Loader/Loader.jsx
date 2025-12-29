65304aaa03a7ad3072b6acdadea051b049b3a878
sohil-P <sohilwork1343@gmail.com>
2025-12-29 18:00:29 +0530

    loader changed

--- src/Components/Loader/Loader.jsx
+++ src/Components/Loader/Loader.jsx
@@ -1,10 +1,11 @@
 const Loader = () => {
   return (
-    <div className="fixed inset-0 flex items-center justify-center bg-[#f0f8ff] dark:bg-[#0c1216] z-50">
-      <div className="relative w-16 h-16">
-        <div className="absolute inset-0 rounded-full bg-gradient-to-r from-[#001c6d] to-[#ec0405] animate-ping"></div>
-        <div className="absolute inset-2 rounded-full bg-gradient-to-r from-[#001c6d] to-[#ec0405] animate-ping delay-150"></div>
-        <div className="absolute inset-4 rounded-full bg-gradient-to-r from-[#001c6d] to-[#ec0405]"></div>
+    <div className="fixed inset-0 flex items-center justify-center bg-white dark:bg-black z-50">
+      <div className="relative w-16 h-16 animate-spin">
+        <div className="absolute w-3 h-3 bg-red-500 rounded-full top-0 left-1/2 -translate-x-1/2"></div>
+        <div className="absolute w-3 h-3 bg-blue-500 rounded-full bottom-0 left-1/2 -translate-x-1/2"></div>
+        <div className="absolute w-3 h-3 bg-green-500 rounded-full left-0 top-1/2 -translate-y-1/2"></div>
+        <div className="absolute w-3 h-3 bg-yellow-500 rounded-full right-0 top-1/2 -translate-y-1/2"></div>
       </div>
     </div>
   );
